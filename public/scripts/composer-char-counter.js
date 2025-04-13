6f7e6d512b5bc4b33996fb2602826fe8e95aa872
Jason Chen <windson7@gmail.com>
2025-04-12 23:44:49 -0700

    use jquery to capture text length and use js to update remaining count dynamically

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  //test code
+  //console.log('ready!');
+  $("#tweet-text").on('input', function() {
+    const maxChars = 140;
+    const counter= $(this).parent().find('.counter');
+    const tweetLength = $(this).val().length;
+    const remainingCount = maxChars - tweetLength;
+    counter.text(remainingCount);
+    if (remainingCount < 0) {
+      counter.css('color', '#FF0000') ;
+    } else {
+      counter.css('color', ''); // Reset color to default if within limit
+    }
+  });
+});
