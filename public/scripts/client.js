b84014e35a9511bc2f2bed3e2f6d82335860ac82
Jason Chen <windson7@gmail.com>
2025-04-13 21:45:00 -0700

    implement functions to create new tweet and render it

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,59 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
+const renderTweets = function(tweets) {
+// loops through tweets
+// calls createTweetElement for each tweet
+// takes return value and appends it to the tweets container
+  for (let tweet of tweets) {
+    createTweetElement(tweet);
+    $('#tweets-container').append($tweet); 
+  }
+}
+
+const createTweetElement = function(tweet) {
+let $tweet = $(
+  ` <article class="tweet">
+      <header>
+        <div><img src="${tweet.user.avatars}"></img></div>
+        <div>${tweet.user.name}</div>
+      </header>
+      <p>${tweet.content.text}</p>
+      <footer>
+        <div>${tweet.created_at}</div>
+        <div class="icon">
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </div>
+      </footer>
+    </article>`
+  );
+
+return $tweet;
+}
+
+renderTweets(data);
