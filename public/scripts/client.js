77397878192ac4a0564e484effcb94d561060b51
Jason Chen <windson7@gmail.com>
2025-04-25 21:02:43 -0700

    fix bug of laoding new page after submission

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,28 +1,3 @@
-// // Fake data taken from initial-tweets.json
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ]
 
 const renderTweets = function(tweets) {
 // loops through tweets
@@ -30,9 +5,15 @@ const renderTweets = function(tweets) {
 // takes return value and appends it to the tweets container
   for (let tweet of tweets) {
     let $tweet = createTweetElement(tweet);
-    $('#tweets-container').append($tweet); 
+    $('#tweets-container').prepend($tweet); 
   }
 }
+
+const escape = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
 
 const createTweetElement = function(tweet) {
   let timeAgo = timeago.format(tweet.created_at);
@@ -45,7 +26,7 @@ const createTweetElement = function(tweet) {
         </header>
         <p>${escape(tweet.content.text)}</p>
         <footer>
-          <div>${timeAgo})</div>
+          <div>${timeAgo}</div>
           <div class="icon">
             <i class="fa-solid fa-flag"></i>
             <i class="fa-solid fa-retweet"></i>
@@ -58,29 +39,31 @@ const createTweetElement = function(tweet) {
 return $tweet;
 }
 
+$(document).ready(function() {
 
-$("#tweet-box").on('submit', function (event) {
-  event.preventDefault();
-  let $serializedTweet = $(this).serialize();
-  //test code
-  //console.log($serializedTweet);
-  const $tweetToSend = $(this).find("textarea").val().trim();
+  $("#tweet-box").on('submit', function (event) {
 
-  if (!validateTweet($tweetToSend)) {
-    return false;
-  }
+    event.preventDefault();
+    let serializedTweet = $(this).serialize();
+    const $tweetToSend = $(this).find("textarea").val().trim();
 
-  $.ajax({
-    url: '/api/tweets',
-    method: 'POST',
-    data: $serializedTweet,
-    success:function(response) {
-      console.log('Tweet submitted successfully:', response);
-      $("#new-tweet").trigger("reset");
-      $("#tweets-container").empty();
-      loadTweets();
+    if (!validateTweet($tweetToSend)) {
+      return false;
     }
-  })
+
+    $.ajax({
+      url: '/api/tweets',
+      method: 'POST',
+      data: serializedTweet,
+      success:function(response) {
+        console.log('Tweet submitted successfully:', response);
+        $("#new-tweet").trigger("reset");
+        $("#tweets-container").empty();
+        loadTweets();
+      }
+    });
+
+  });
 
 });
 
