c7d082849306440ca50b6c763e7ddb99ab072e1c
Jason Chen <windson7@gmail.com>
2025-04-28 21:55:44 -0700

    add comments and reset counter on successful submission

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -9,13 +9,18 @@ const renderTweets = function(tweets) {
   }
 }
 
+//escapes a string for HTML output, preventing Cross-Site Scripting
+
 const htmlEscape = function (str) {
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 };
 
+//create new tweet as html element
 const createTweetElement = function(tweet) {
+
+  //convert timestamp of time created into readable time string (eg. "1 day ago")
   let timeAgo = timeago.format(tweet.created_at);
     
   let $tweet = $(
@@ -43,8 +48,11 @@ return $tweet;
 $(document).ready(function() {
 
   $("#tweet-box").on('submit', function (event) {
-
+    
+    //prevent from propogation 
     event.preventDefault();
+
+    //create a URL-encoded string for AJAX and trim for validation
     let serializedTweet = $(this).serialize();
     const $tweetToSend = $(this).find("textarea").val().trim();
 
@@ -52,13 +60,18 @@ $(document).ready(function() {
       return false;
     }
 
+    //ajax allows part of page to update on successful tweet submission
     $.ajax({
       url: '/api/tweets',
       method: 'POST',
       data: serializedTweet,
       success:function(response) {
         console.log('Tweet submitted successfully:', response);
+        //reset textarea
         $("#tweet-box").trigger("reset");
+        //reset counter
+        $('.counter').text(140).css('color', '');
+        //add new tweet top of tweets
         const $tweetElement = createTweetElement(response);
         $("#tweets-container").prepend($tweetElement);
       }
@@ -68,7 +81,7 @@ $(document).ready(function() {
 
 });
 
-
+//validate tweet to check if string length is between 0 and 140
 const validateTweet = function(tweet) {
 
   if (tweet.length === 0) {
@@ -86,6 +99,7 @@ const validateTweet = function(tweet) {
 
 }
 
+//render all tweets on successful GET request
 const loadTweets = function() {
   $.ajax({
     url: 'api/tweets',
@@ -94,11 +108,12 @@ const loadTweets = function() {
   })
 }
 
-
+//show error with slide animation
 const showError = function(message) {
   $("#tweet-error").text(message).hide().slideDown();
 }
 
+//hide error (on successful submission)
 const clearError = function() {
   $("#tweet-error").hide().text('');
 }
